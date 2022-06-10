6abab86271d40bb874c69cea4518c9cce02942e1
Enivri <Irvineidris24@gmail.com>
2022-06-10 21:14:10 +0700

    commit

--- config/database.js
+++ config/database.js
@@ -1,20 +1,31 @@
+const config = new URL(
+  process.env.DATABASE_URL ||
+      "postgres://postgres:irvine123@127.0.0.1:5432/bcr"
+)
+
 const {
-  DB_USER = "postgres",
-  DB_PASSWORD = "irvine123",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
+  DB_USER = config.username,
+  DB_PASSWORD = config.password,
+  DB_NAME = config.pathname.replace("/", ""),
+  DB_HOST = config.hostname,
   DB_PORT = "5432",
-} = process.env;
+} = process.env
 
 module.exports = {
   development: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres",
-    logging: false,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      host: DB_HOST,
+      port: DB_PORT,
+      ssl: true,
+      dialectOptions: {
+          ssl: {
+              require: true,
+              rejectUnauthorized: false,
+          },
+      },
+      dialect: "postgres",
   },
   test: {
     username: DB_USER,
@@ -22,17 +33,23 @@ module.exports = {
     database: `${DB_NAME}_test`,
     host: DB_HOST,
     port: DB_PORT,
-    // dialect: "postgres",
     storage: "node_modules/test.sqlite",
     dialect: "sqlite",
     logging: false,
   },
   production: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: "postgres"
-  }
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      host: DB_HOST,
+      port: DB_PORT,
+      ssl: true,
+      dialectOptions: {
+          ssl: {
+              require: true,
+              rejectUnauthorized: false,
+          },
+      },
+      dialect: "postgres",
+  },
 }
